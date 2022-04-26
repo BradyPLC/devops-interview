4743ccc66bb7f966c4c4e366c8795c49af8dc1ec
Ravish Jain <ravishjain.it@gmail.com>
2022-04-26 02:43:57 +0100

    Added Integration tests

---
+++ BradyWeather.Tests/Core/CoreTests.cs
@@ -0,0 +1,82 @@
+using BradyWeather.Application.Interfaces;
+using BradyWeather.Application.UseCases.Weather.Handlers;
+using BradyWeather.Common;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Refit;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BradyWeather.Tests
+{
+    [TestClass]
+    public class CoreTests
+    {
+        IConfiguration config;
+
+        public CoreTests()
+        {
+            config = InitConfiguration();
+        }
+
+        [TestMethod]
+        public async Task GetLocationsSearchHandler_Handle_Test()
+        {
+            var weatherApiUrl = config["Web:WeatherApi:BaseAddress"];
+            var weatherApiKey = config["Web:WeatherApi:ApiKey"];
+
+            var streamingWeatherCTS = new CancellationTokenSource();
+            var weatherClient = RestService.For<IWeatherClient>(weatherApiUrl);
+            var logger = new Mock<ILogger<GetLocationsSearchHandler>>();
+            var weatherSettings = new WeatherSettings() {
+                ApiKey = weatherApiKey,
+                BaseAddress = new System.Uri(weatherApiUrl)
+            };
+            var monitor = Mock.Of<IOptionsMonitor<WeatherSettings>>(_ => _.CurrentValue == weatherSettings);
+            var handler = new GetLocationsSearchHandler(logger.Object, weatherClient, monitor);
+
+            var weatherResponse = await handler.Handle(new GetLocationsByTextRequest() { Search = "London"}, streamingWeatherCTS.Token);
+
+            Assert.IsNotNull(weatherResponse);
+            Assert.IsTrue(weatherResponse.Length>0);
+            Assert.IsTrue(weatherResponse.Select(x=>x.LocalizedName == "London").ToList().Count()>0);
+        }
+
+        [TestMethod]
+        public async Task GetWeatherForecastHandler_Handle_Test()
+        {
+            var weatherApiUrl = config["Web:WeatherApi:BaseAddress"];
+            var weatherApiKey = config["Web:WeatherApi:ApiKey"];
+
+            var streamingWeatherCTS = new CancellationTokenSource();
+            var weatherClient = RestService.For<IWeatherClient>(weatherApiUrl);
+            var logger = new Mock<ILogger<GetLocationsSearchHandler>>();
+            var weatherSettings = new WeatherSettings()
+            {
+                ApiKey = weatherApiKey,
+                BaseAddress = new System.Uri(weatherApiUrl)
+            };
+            var monitor = Mock.Of<IOptionsMonitor<WeatherSettings>>(_ => _.CurrentValue == weatherSettings);
+            var handler = new GetLocationsSearchHandler(logger.Object, weatherClient, monitor);
+
+            var weatherResponse = await handler.Handle(new GetLocationsByTextRequest() { Search = "London" }, streamingWeatherCTS.Token);
+
+            Assert.IsNotNull(weatherResponse);
+            Assert.IsTrue(weatherResponse.Length > 0);
+            Assert.IsTrue(weatherResponse.Select(x => x.LocalizedName == "London").ToList().Count() > 0);
+        }
+
+        public static IConfiguration InitConfiguration()
+        {
+            var config = new ConfigurationBuilder()
+               .AddJsonFile("appsettings.Tests.json")
+                .AddEnvironmentVariables()
+                .Build();
+            return config;
+        }
+    }
+}
